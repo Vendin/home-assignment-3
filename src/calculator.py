868bf740ee25931f35c87e37cf8c9663e3c09f1d
Vendin <v.a.5@list.ru>
2015-11-14 21:58:42 +0300

    fix and setUp

--- src/calculator.py
+++ src/calculator.py
@@ -3,19 +3,18 @@ from decimal import Decimal
 
 class Calculator:
     def addition(self, x, y):
-        return x + y
+        return float(Decimal(str(x)) + Decimal(str(y)))
 
     def subtraction(self, x, y):
         return float(Decimal(str(x)) - Decimal(str(y)))
 
     def multiply(self, x, y):
-        return x * y
+        return float(Decimal(str(x)) * Decimal(str(y)))
 
     def divide(self, x, y):
         if y == 0:
             raise ValueError('ZeroDivisionError')
-        return float(x) / float(y)
-
+        return float(Decimal(str(x)) / Decimal(str(y)))
 
     def logMy(self, x, y = math.e):
         if y <= 0 or x <= 0:
@@ -33,11 +32,6 @@ class Calculator:
                 z += 0.000001
              return round(z, 5)
 
-
     def logMath(self, x, y):
         return math.log(x, y)
 
-
-#
-# cal = Calculator()
-# print(cal.subtraction(0.3, 0.1))
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,72 +1,63 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from tests.calculator import Calculator
+
+from src.calculator import Calculator
+
 
 class CalculatorTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.cal = Calculator()
+
     def testAddition(self):
-        cal = Calculator()
-        result = cal.addition(0.001, 0.003)
+        result = self.cal.addition(0.001, 0.003)
         self.assertEqual(0.004, result)
 
     def testSubtraction(self):
-        cal = Calculator()
-        result = cal.subtraction(0.1, 0.001)
+        result = self.cal.subtraction(0.1, 0.001)
         self.assertEqual(0.099, result)
 
     def testMultiply(self):
-        cal = Calculator()
-        result = cal.multiply(0.001, 10)
+        result = self.cal.multiply(0.001, 10)
         self.assertEquals(0.01, result)
 
     def testDivide(self):
-        cal = Calculator()
-        result = cal.divide(5, 1000)
+        result = self.cal.divide(5, 1000)
         self.assertEquals(0.005, result)
 
     def testDivideError(self):
-        cal = Calculator()
-        self.assertRaises(ValueError, cal.divide, 10, 0)
+        self.assertRaises(ValueError, self.cal.divide, 10, 0)
 
     def testLogMyMore(self):
-        cal = Calculator()
-        result = cal.logMy(25, 5)
+        result = self.cal.logMy(25, 5)
         self.assertEquals(2, result)
 
     def testLogMyLess(self):
-        cal = Calculator()
-        result = cal.logMy(0.125, 2)
+        result = self.cal.logMy(0.125, 2)
         self.assertEquals(-3, result)
 
     def testLog(self):
-        cal = Calculator()
-        log1 = cal.logMath(12223, 2)
-        log2 = cal.logMy(12223, 2)
+        log1 = self.cal.logMath(0.221, 2.5)
+        log2 = self.cal.logMy(0.221, 2.5)
         self.assertEquals(round(log1, 5), log2)
 
     def testLogMyErrorOne(self):
-        cal = Calculator()
-        self.assertRaises(ValueError, cal.logMy, 100, 0)
+        self.assertRaises(ValueError, self.cal.logMy, 100, 0)
 
     def testLogMyErrorTwo(self):
-        cal = Calculator()
-        self.assertRaises(ZeroDivisionError, cal.logMy, 100, 1)
+        self.assertRaises(ZeroDivisionError, self.cal.logMy, 100, 1)
 
     def testLogMathMore(self):
-        cal = Calculator()
-        result = cal.logMath(25, 5)
+        result = self.cal.logMath(25, 5)
         self.assertEquals(2, result)
 
     def testLogMathLess(self):
-        cal = Calculator()
-        result = cal.logMath(0.125, 2)
+        result = self.cal.logMath(0.125, 2)
         self.assertEquals(-3, result)
 
     def testLogMathErrorOne(self):
-        cal = Calculator()
-        self.assertRaises(ValueError, cal.logMath, 100, 0)
+        self.assertRaises(ValueError, self.cal.logMath, 100, 0)
 
     def testLogMathErrorTwo(self):
-        cal = Calculator()
-        self.assertRaises(ZeroDivisionError, cal.logMath, 100, 1)
+        self.assertRaises(ZeroDivisionError, self.cal.logMath, 100, 1)
